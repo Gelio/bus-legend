6dcfc1d531da473c1b7752d55c7fe8b0bd24ae7d
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2024-03-24 13:53:59 +0100

    Revert "chore: install UnoCSS"
    This reverts commit f079e6c97672accee27f8fa99cd9f8ff02ea4e2f.
    
    UnoCSS does not have a good-enough Neovim LSP.

--- src/routes/+layout.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import 'uno.css';
-</script>
-
-<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p class="text-green-600">
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
