7ee60c576016d97ecb5a9cffd0b3b69b7b878990
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2024-03-24 14:10:17 +0100

    feat: basic displaying of departures for a given date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="text-2xl">
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script lang="ts">
+	import { krasnystawDepartures } from '$lib/bus-lines/data/lublin/lines/krasnystaw';
+	import { Temporal } from 'temporal-polyfill';
+
+	let date = $state(Temporal.Now.plainDateISO().toString());
+
+	let departuresOnDate = $derived.by(() => {
+		if (date === '') {
+			return [];
+		}
+
+		const plainDate = Temporal.PlainDate.from(date);
+		return krasnystawDepartures.filter((departure) =>
+			departure.conditions.every((condition) => condition.filter(plainDate)),
+		);
+	});
+</script>
+
+<input type="date" bind:value={date} />
+
+<ul>
+	{#each departuresOnDate as departure}
+		<li>{departure.time.toString({ smallestUnit: 'minute' })}</li>
+	{/each}
+</ul>
