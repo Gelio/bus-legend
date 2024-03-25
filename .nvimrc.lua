e58c2e0f93da629b0133563ae259abd6de746153
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2024-03-25 10:57:08 +0100

    chore(nvim): improve tailwindcss autocomplete with dashes

--- .nvimrc.lua
+++ .nvimrc.lua
@@ -2,3 +2,11 @@ local utils = require("lsp.utils")
 local default_server_config = utils.base_config
 
 require("lspconfig").tailwindcss.setup(default_server_config)
+
+require("cmp").setup.filetype("svelte", {
+	sources = require("lsp.cmp").prepare_sources({
+		-- NOTE: keeps the autocomplete for tailwindcss classes that include dash
+		-- https://github.com/xna00/unocss-language-server?tab=readme-ov-file#usage
+		lsp_trigger_characters = { "-" },
+	}),
+})
